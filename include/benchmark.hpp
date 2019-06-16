7ac66d1bdd9cf486468eccf5bb32066fc4023ed8
Michail Safronov <msaf1980@gmail.com>
2019-06-16 08:05:01 +0500

    fix

--- include/benchmark.hpp
+++ include/benchmark.hpp
@@ -1,6 +1,7 @@
 #ifndef _BENCHMARK_HPP_
 #define _BENCHMARK_HPP_
 
+#include <math.h>
 #include <stdio.h>
 
 #include <algorithm>
@@ -28,38 +29,55 @@ namespace benchmark {
 class Benchmark;
 
 struct BenchmarkStat {
-	uint64_t max;
-	uint64_t min;
-	uint64_t avg;
-	uint64_t p95;
-	uint64_t p99;
-	uint64_t div_min;
-	uint64_t div_max;
+	int64_t max;
+	int64_t min;
+	int64_t avg;
+	int64_t p90;
+	int64_t p95;
+	int64_t p99;
+	int64_t div_min;
+	int64_t div_max;
 };
 
-uint64_t percentile(const std::vector<uint64_t> &r, size_t pcnt) {
-	size_t size = r.size();
+int64_t percentile(std::vector<int64_t>::const_iterator b,
+                   std::vector<int64_t>::const_iterator e, size_t pcnt) {
+	size_t size = std::distance(b, e);
 	if (size > 1) {
 		size_t i = pcnt * size / 100;
-		if (size % 2 == 0) {
-			return (r[i] + r[i + 1]) / 2;
+		if (size % 2 == 0 && i < size - 2) {
+			return (*std::next(b, i) + *std::next(b, i + 1)) / 2;
 		} else {
-			return r[i];
+			return *std::next(b, i);
 		}
 	} else if (size == 1) {
-		return r[0];
+		return *b;
 	} else
 		return 0;
 }
 
-BenchmarkStat calc_stat(std::vector<uint64_t> &r) {
-	BenchmarkStat stat = {0, 0, 0, 0, 0, 0, 0};
+BenchmarkStat calc_stat(std::vector<int64_t> &r) {
+	BenchmarkStat stat = {0, 0, 0, 0, 0, 0, 0, 0};
 	std::sort(r.begin(), r.end());
 	stat.max = r[r.size() - 1];
 	stat.min = r[0];
-	stat.avg = percentile(r, 50);
-	stat.p95 = percentile(r, 95);
-	stat.p99 = percentile(r, 99);
+
+	size_t last = r.size() - 1;
+	/* trunc high deviated result */
+	while (last > 20 && last > r.size() - 5) {
+		stat.p95 = percentile(r.cbegin(), std::next(r.cbegin(), last + 1), 95);
+		stat.div_max = stat.max - stat.p95;
+		if (stat.div_max > stat.p95)
+			last--;
+		else
+			break;
+	}
+
+	auto end_it = std::next(r.cbegin(), last + 1);
+	stat.max = r[last];
+	stat.avg = percentile(r.cbegin(), end_it, 50);
+	stat.p90 = percentile(r.cbegin(), end_it, 90);
+	stat.p95 = percentile(r.cbegin(), end_it, 95);
+	stat.p99 = percentile(r.cbegin(), end_it, 99);
 	stat.div_min = stat.p95 - stat.min;
 	stat.div_max = stat.max - stat.p95;
 	return stat;
@@ -87,7 +105,7 @@ class Benchmark {
 				for (size_t i = 0; i < iterations; i++) {
 					int64_t d = bench();
 					if (d >= 0) {
-						duration += d / 1000;
+						duration += d;
 					} else {
 						throw std::runtime_error("negative duration");
 					}
@@ -111,14 +129,14 @@ class Benchmark {
 			r->report(this);
 	}
 
-	std::string           group;
-	std::string           name;
-	size_t                threads;
-	size_t                samples;
-	size_t                iterations;
-	bool                  success;
-	std::string           err;
-	std::vector<uint64_t> durations;
+	std::string          group;
+	std::string          name;
+	size_t               threads;
+	size_t               samples;
+	size_t               iterations;
+	bool                 success;
+	std::string          err;
+	std::vector<int64_t> durations;
 
 	BenchmarkReporter *r;
 };
@@ -126,9 +144,11 @@ class Benchmark {
 class BenchmarkStdoutReporter : public BenchmarkReporter {
   public:
 	BenchmarkStdoutReporter() {
-		printf("%10s | %10s | %8s | %10s | %10s | %14s | %14s | %14s |\n",
+		std::string s_delim(135, '_');
+		printf("%s\n", s_delim.c_str());
+		printf("%10s | %10s | %8s | %10s | %10s | %14s | %14s | %14s | %20s |\n",
 		       "Group", "Benchmark", "Threads", "Samples", "Iterations",
-		       "us/Iter P95", "P99", "Div Min/Max");
+		       "us/Iter P90" ,"P95", "P99", "P95 Div Min/Max");
 	}
 
 	virtual void report(Benchmark *b) {
@@ -136,16 +156,21 @@ class BenchmarkStdoutReporter : public BenchmarkReporter {
 		       b->name.c_str(), b->threads, b->samples, b->iterations);
 		if (b->success && !b->durations.empty()) {
 			BenchmarkStat stat = calc_stat(b->durations);
-			std::string div = (stat.div_min == 0 ? "" : "-") + std::to_string(stat.div_min) + "/" + std::to_string(stat.div_max);
-			printf(" %14lu | %14lu | %14s | %14lu\n", stat.p95,
-			       stat.p99, div.c_str(), stat.min);
+/*
+			std::cout << std::endl;
+			for (auto r : b->durations) {
+				std::cout << r << std::endl;
+			}
+*/
+			std::string div = (stat.div_min == 0 ? "" : "-") +
+			                  std::to_string(stat.div_min) + "/" +
+			                  std::to_string(stat.div_max);
+			printf(" %14lu | %14lu | %14lu | %20s |\n", stat.p90,
+			       stat.p95, stat.p99, div.c_str());
 		} else if (b->err.empty()) {
 			printf(" SKIP\n");
 		} else {
 			printf(" FAIL: %s\n", b->err.c_str());
-		}
-		for (auto r: b->durations) {
-			std::cout << r << std::endl;
 		}
 	}
 };
